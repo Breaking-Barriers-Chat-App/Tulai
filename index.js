fc4ce5da86c18fe36407fe6e824cddeea9c3400a
Georgin Maliakal <georgin@Georgins-MacBook-Pro.local>
2024-01-06 15:34:48 -0700

    highlighted text works

---
+++ index.js
@@ -0,0 +1,17 @@
+// Following snippet to select text taken from https://stackoverflow.com/questions/68542700/getting-selected-text-in-a-chrome-extension
+// Author: Kiaran
+
+document.getElementById("save-btn").onclick = async () => {
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+    let result;
+    try {
+      [{result}] = await chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: () => getSelection().toString(),
+      });
+    } catch (e) {
+      return; // ignoring an unsupported page like chrome://extensions
+    }
+    document.body.append('Selection: ' + result);
+  };
+
